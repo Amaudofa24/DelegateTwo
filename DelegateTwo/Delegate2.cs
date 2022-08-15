2d9c7cc60677241c045f6c72ed4454b6f25d8111
Support <Support@WEMA-TRAINING-S>
2022-08-15 23:23:34 +0100

    Add project files.

---
+++ DelegateTwo/Delegate2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateTwo
+{
+    internal class Delegate2
+    {
+        public delegate void Reel(int percentConcluded);
+        public static void ReelUploader(Reel re)
+        {
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    var Concluded = i.ToString();
+                    re(i);
+                }
+            }
+        }
+        static void TryAnonymous()
+        {
+            Reel Ig = delegate (int percent)
+            {
+                var answer = percent / 100;
+                Console.WriteLine(answer);
+            };
+        }
+
+    }
+}
---
+++ DelegateTwo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DelegateTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Delegate2 de = new Delegate2();
+        }
+    }
+}
